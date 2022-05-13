b55f219950b766c327df5b2c7278a036296671fd
Marlon James <marlonjamesdev@gmail.com>
2022-05-13 03:51:22 -0300

    Stage 3 finished

--- js/scripts.js
+++ js/scripts.js
@@ -13,3 +13,18 @@ function openMenu() {
 function closeMenu() {
     document.body.classList.remove('menu-expanded');
 }
+
+ScrollReveal({
+    origin: 'top',
+    distance: '30px',
+    duration: 700,
+}).reveal(`
+#home, 
+#home img, 
+#home .stats, 
+#services,
+#services header,
+#services .card,
+#about,
+#about header,
+#about .content`);
